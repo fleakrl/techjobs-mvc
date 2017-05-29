5579a15298fea458411f0cb2b38d302a4fc2ede0
Rebecca Fleak <fleakrl@gmail.com>
2017-05-29 16:11:52 -0500

    implemented endpoint - TODO #1 search

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,16 +4,19 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("search")
+@RequestMapping("/search")
 public class SearchController {
 
     @RequestMapping(value = "")
@@ -22,6 +25,13 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "/results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm ){
+
+        ArrayList<HashMap<String, String>> searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+
+        model.addAttribute("searchResults",searchResults);
+        return "search";
+    }
 
 }
